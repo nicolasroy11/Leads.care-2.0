d375c932605555617fe276559ac953c771cb0ea1
nicolasroy11 <nicolasroy11@gmail.com>
2018-02-05 18:58:29 -0800

    Cleaned up Interfaces

--- Shared/Interfaces/IDataSyncRepository.ts
+++
@@ -1,8 +0,0 @@
-import { DataSyncModel } from '../Models/DataSyncModel';
-
-export interface IDataSyncRepository {
-    GetUnsyncedLocalRecords(maxRecords: number): DataSyncModel;
-    UpdateUnsyncedLocalRecords(recordKeys: any, transferDt: any): void;
-    AddOrUpdateLocalRecords(dataSyncModel: DataSyncModel): void;
-    AddOrUpdateRemoteRecords(dataSyncModel: DataSyncModel): void;
-}
