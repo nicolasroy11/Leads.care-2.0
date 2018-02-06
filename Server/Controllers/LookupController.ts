a33caa9b25e7806bfcf8814c94846e9810bc5dc6
nicolasroy11 <nicolasroy11@gmail.com>
2018-02-05 19:13:56 -0800

    TestController Deleted; sections commented out temporarily to avoid errors

--- Server/Controllers/LookupController.ts
+++ Server/Controllers/LookupController.ts
@@ -2,14 +2,14 @@ import { JsonController, Post, Body, QueryParam } from 'routing-controllers';
 import { LookupTypes } from '../../Shared/Enums/LookupTypes';
 import { PaginatedSearchResultModel } from '../../Shared/Models/PaginatedSearchResultModel';
 import { LookupSearchCriteriaModel } from '../../Shared/Models/LookupSearchCriteriaModel';
-import { LookupRepository } from '../Repositories.Mock/LookupRepository';
+// import { LookupRepository } from '../Repositories.Mock/LookupRepository';
 
 @JsonController('/Lookup')
 export class LookupController {
-    @Post('/Search')
-    public Search(@Body() searchCriteria: LookupSearchCriteriaModel): Promise<PaginatedSearchResultModel> {
-        let repository: LookupRepository = new LookupRepository();
-        let response: Promise<PaginatedSearchResultModel> = repository.Search(searchCriteria);
-        return response;
-    }
+    // @Post('/Search')
+    // public Search(@Body() searchCriteria: LookupSearchCriteriaModel): Promise<PaginatedSearchResultModel> {
+    //     let repository: LookupRepository = new LookupRepository();
+    //     let response: Promise<PaginatedSearchResultModel> = repository.Search(searchCriteria);
+    //     return response;
+    // }
 }
--- Server/Test/Repositories.Mongo/LeadsDBInit.ts
+++ Server/Test/Repositories.Mongo/LeadsDBInit.ts
@@ -1,39 +1,39 @@
-import { expect } from 'chai';
-import { ISuiteCallbackContext } from 'mocha';
-import * as mongoose from 'mongoose';
-import { LeadSchema } from '../../Schema/Leads.Schema';
-import { UserSchema } from '../../Schema/Users.Schema';
-import { SampleData } from '../../Repositories.Mock/SampleData';
-import { DictionarySearchResultModel } from '../../../Shared/Models/DictionarySearchResultModel';
-import { PhoneNumberModel } from '../../../Shared/Models/PhoneNumberModel';
-import { LeadModel } from '../../../Shared/Models/LeadModel';
-import { UserModel } from '../../../Shared/Models/UserModel';
-import { Database } from '../../Framework/Database';
+// import { expect } from 'chai';
+// import { ISuiteCallbackContext } from 'mocha';
+// import * as mongoose from 'mongoose';
+// import { LeadSchema } from '../../Schema/Leads.Schema';
+// import { UserSchema } from '../../Schema/Users.Schema';
+// import { SampleData } from '../../Repositories.Mock/SampleData';
+// import { DictionarySearchResultModel } from '../../../Shared/Models/DictionarySearchResultModel';
+// import { PhoneNumberModel } from '../../../Shared/Models/PhoneNumberModel';
+// import { LeadModel } from '../../../Shared/Models/LeadModel';
+// import { UserModel } from '../../../Shared/Models/UserModel';
+// import { Database } from '../../Framework/Database';
 
-describe('Server.LeadsDBInit', function () {
-    this.timeout(3000);
-    let sampleData = new SampleData();
-    let DB = new Database();
-    DB.Initialize();
+// describe('Server.LeadsDBInit', function () {
+//     this.timeout(3000);
+//     let sampleData = new SampleData();
+//     let DB = new Database();
+//     DB.Initialize();
 
-    it('should enter several mock leads in Mongo', function (done) {
-        // patchy code, needs to be refactored
-        let leads = sampleData.GenerateLeads();
-        let user = new UserModel();
-        user.Name = 'nick';
-        user.Username = 'nick';
-        user.Password = 'nick';
-        user.Email = 'n@n.com';
-        let newUser = new UserSchema(user);
-        newUser.save((err1, savedUser) => {
-            for (let i: number = 0; i < leads.length; i++) {
-                leads[i].UserId = <number><any>savedUser.UserId;
-                let leadsModel = new LeadSchema(leads[i]);
-                leadsModel.save((err2, savedLeads) => {
-                    let s = savedLeads;
-                });
-            }
-        });
+//     it('should enter several mock leads in Mongo', function (done) {
+//         // patchy code, needs to be refactored
+//         let leads = sampleData.GenerateLeads();
+//         let user = new UserModel();
+//         user.Name = 'nick';
+//         user.Username = 'nick';
+//         user.Password = 'nick';
+//         user.Email = 'n@n.com';
+//         let newUser = new UserSchema(user);
+//         newUser.save((err1, savedUser) => {
+//             for (let i: number = 0; i < leads.length; i++) {
+//                 leads[i].UserId = <number><any>savedUser.UserId;
+//                 let leadsModel = new LeadSchema(leads[i]);
+//                 leadsModel.save((err2, savedLeads) => {
+//                     let s = savedLeads;
+//                 });
+//             }
+//         });
 
-    });
-});
+//     });
+// });
